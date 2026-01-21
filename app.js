fddcb16f974c1cf9b4071fc8b56bcb22d58b39c2
ajay <22cs10aj88@mitsgwl.acin>
2026-01-21 19:03:11 +0530

    Fix env + mongodb connection + deployment ready

--- app.js
+++ app.js
@@ -83,14 +83,7 @@ app.use((req,res,next) =>{
     res.locals.currUser = req.user;
     next();
 })
-// app.get("/demouser", async (req,res) =>{
-//     let fakeUser = new User({
-//         email : "yuvraj@gmaol.com",
-//         username : "yuvraj",
-//     });
-//     let registeredUser = await User.register(fakeUser,"helloworld");
-//     res.send(registeredUser);
-// });
+
 
 app.use("/listings",listingRouter);
 app.use("/listings/:id/review",reviewRouter);
